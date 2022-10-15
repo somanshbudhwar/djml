3a4a5da1b2bcd98c8d2e0ccc0c79d37a81daae8e
somanshbudhwar <somanshbudhwar@gmail.com>
2022-10-15 13:40:17 -0700

    Linked domain from Route 53 to EC2

--- ml/ml/settings.py
+++ ml/ml/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.139.6.237','ec2-18-191-183-35.us-east-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['finance.somanshbudhwar.com','3.139.6.237','ec2-18-191-183-35.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
