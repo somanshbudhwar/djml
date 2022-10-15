ee9010097cbb06fc0720da1e208b7d9f32e1324f
somanshbudhwar <somanshbudhwar@gmail.com>
2022-10-15 10:29:03 -0700

    Changed secret key code

--- ml/ml/settings.py
+++ ml/ml/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%))w1&@23nj$re#ideh8ija_+i1ai7#^y+^f07cby3bygv$b7^'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
